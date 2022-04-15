4264df1135a82752ffd8e80475195469c32515ce
Li Cheng <lilybearcheng@Lis-MacBook-Pro.home>
2022-04-14 17:04:28 -0700

    last working version

--- main.js
+++ main.js
@@ -1,60 +1,46 @@
-//const webList = [];
-function createBlockList() {
-  const input = document.getElementById("websiteURL")
-  //webList.push(input.value + '/*');
+const webList = [];
 
-  chrome.storage.local.set({input: document.location}, function() {
-    //   console.log('Value is set to ' + value);
-    });
-
-  document.getElementById('displayList').innerHTML += ('<li>'+ input.value +'</li>');
-  input.value = '';
-  console.log(displayList);
-}
-
-
-
-
-document.getElementById('addWebsite').addEventListener('click', createBlockList);
-
-
-
-
-// chrome.storage.local.get(['key'], function(result) {
-//   console.log('Value currently is ' + result.key);
-// });
-
-document.addEventListener("DOMContentLoaded", function() {
-  let currentWeb = document.location;
-  console.log("function invoked");
-  //if chrome.storage.local contains currentWeb run the location replace .get(input, function)
-  if (chrome.storage.local.includes(currentWeb)) {
-   currentWeb = location.replace("https://www.codesmith.io/faq#tuition-financing");
-    return currentWeb;
-  }
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('addWebsite').addEventListener('click', addSaveAndDisplayUrl);
+  redirectToCodesmith();
 });
 
 
-//below: rule variable to note when a banned site has been triggered, with actions
-// var bannedSiteRule = {
-// conditions: [
-
-//   //need to make sure that 'RequestMatcher' actually matches what we want it to
-//   new chrome.declarativeWebRequest.RequestMatcher({
-//     //must find a way to make this url suffix pull from the weblist variable above
-//      url: { hostSuffix: 'example.com' } })
-
-//     //lines below would be how to add another url request; comma separates them after }})
-//     //  ,
-//     //  new chrome.declarativeWebRequest.RequestMatcher({
-//     //  url: { hostSuffix: 'foobar.com' } })
-//  ],
-
-//   actions: [
-//     //need to have below action redirect the request to a different URL (code in line 1)
 
 
-//     //may still need to have cancel request?
-// new chrome.declarativeWebRequest.CancelRequest()
-//   ]
-//  };
+
+function addSaveAndDisplayUrl() {
+  //take user input, add url to webList array
+  const inputUrl = document.getElementById("websiteURL");
+  webList.push(inputUrl.value);
+  //display url on popup
+  document.getElementById('displayList').innerHTML += ('<li>'+ inputUrl.value +'</li>');
+  //save webList to chrome
+  chrome.storage.local.set({"urlList" : webList}, function() {
+    console.log("webList saved");
+    console.log({"urlList" : webList});
+  })
+  //clear input box
+  inputUrl.value = '';
+}
+
+function redirectToCodesmith() {
+  //add event listener for crhom,tabs.query, callback is redirect web page
+  chrome.tabs.query({currentWindow: true,active: true}, function(tabs) {
+    //obtain current tab's url
+    let currentUrl = tabs[0].url;
+    console.log(currentUrl);
+    //get webList from chrome
+    chrome.storage.local.get({"urlList" : webList}, function() {
+      console.log("got webList from chrome");
+      console.log(currentUrl);
+      //if url is in webList
+      if (webList.includes(currentUrl)) {
+        //redirect webpage to codesmith
+        tabs[0].location.replace("https://www.codesmith.io/faq#tuition-financing");
+      }
+        
+    })  
+    
+  });
+}
